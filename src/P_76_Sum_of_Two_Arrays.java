b0b423793b345b8632e428918f2ce75ae11e9b34
SGsubuuu <pradhansubham147@gmail.com>
2022-11-29 15:29:36 +0530

    Practice codes

--- src/P_76_Sum_of_Two_Arrays.java
+++ src/P_76_Sum_of_Two_Arrays.java
@@ -59,10 +59,83 @@
 
 import java.util.*;
 public class P_76_Sum_of_Two_Arrays {
+	
+	public static int[] inputArray(Scanner ip, int num){
+		System.out.println("So, now enter the length of your array-" + num + " :- ");
+		int len = ip.nextInt();
+		int [] arr = new int[len];
+		for(int i = 0; i < arr.length; i++){
+			System.out.println("Now, enter the " + i + "th element for your array-" + num + " :- ");
+			arr[i] = ip.nextInt();
+		}
+		
+		return arr;
+	}
+	
+	public static void printArray(int [] arr){
+		System.out.print("(");
+		for(int i = 0; i < arr.length; i++){
+			System.out.print(arr[i]);
+			if(i < arr.length-1)
+				System.out.print(", ");
+		}
+		System.out.print(")");
+		System.out.println();
+	}
+	
+	public static int[] addingArrays(int [] arr1, int [] arr2){
+		int max_len = 0;
+		if(arr1.length > arr2.length){
+			max_len = arr1.length;
+		}
+		else if(arr2.length > arr1.length){
+			max_len = arr2.length;
+		}
+		else if(arr1.length == arr2.length){
+			max_len = arr1.length;
+		}
+		
+		int [] arr3 = new int[max_len + 1];
+		int carry = 0, i = arr1.length - 1, j = arr2.length - 1, k = arr3.length - 1, sum = 0, final_Res = 0;
+		
+		
+		while(i >= 0  && j >= 0){
+			sum = arr1[i] + arr2[j] + carry;
+			carry = sum/10;
+			final_Res = sum % 10;
+			i--;
+			j--;
+			arr3[k] = final_Res;
+			k--;
+		}
+		
+		while(i >= 0){
+			arr3[k] = arr1[i];
+			i--;
+			k--;
+		}
+		
+		while(j >= 0){
+			arr3[k] = arr2[j];
+			j--;
+			k--;
+			
+		}
+		
+		return arr3;
+	}
 
 	public static void main(String[] args) {
 		Scanner ip = new Scanner(System.in);
-
+		System.out.println("This is the program for adding and showing  indices of two arrays in normal addition...");
+	    int [] arr1 = inputArray(ip, 1);
+	    int [] arr2 = inputArray(ip, 2);
+	    System.out.println("So, your entered arrays is as follows :- ");
+	    printArray(arr1);
+	    printArray(arr2);
+	    System.out.println("And, after adding both of  your arrays the resultant array is as follows :- ");
+	    int [] arr3 = addingArrays(arr1, arr2);
+	    printArray(arr3);
 	}
 
 }
